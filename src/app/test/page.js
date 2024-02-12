3e40e2a8ec37bc92f268c9e27cf9a70c93e6bf1e
quentinkerzerho <quentin.kerzerho@gmail.com>
2024-02-12 12:32:15 +0100

    test2

---
+++ src/app/test/page.js
@@ -0,0 +1,23 @@
+// pages/test.js
+import React from "react";
+import prisma from "@/lib/prisma";
+import ResponsiveAppBar from "@/src/components/ResponsiveAppBar";
+
+export default async function Test() {
+  // const feed = await prisma.Name.findMany();
+
+  return (
+    <div>
+      <main>
+        <ResponsiveAppBar />
+        <p>Je veux la donnée de name là</p>
+        {/* Utilisez les données de feed ici */}
+        {feed.map((item) => (
+          <div key={item.name}>
+            <p>Name: {item.name}</p>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
+}
